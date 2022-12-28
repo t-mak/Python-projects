6c133e2cc22fda03d635088262569226b4da0b82
t-mak <makowski.t91@gmail.com>
2022-12-28 16:53:27 +0900

    Delete unneccesary variable

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,10 @@
 
 given_array = [3, 1, 5, 7, 2, 4, 4, 4, 9, 8, 7]
-sorted_array = []
 
 length_array = len(given_array)
-n = length_array
 turn = 0
 
-while (n>1):
+while (length_array>1):
     swapped = False
     turn = turn + 1
     for j in range(0, length_array-1):
@@ -19,7 +17,7 @@ while (n>1):
             given_array[j+1] = temp
             print(given_array)
             swapped = True
-    n = n - 1
+    length_array = length_array - 1 #since the highest value element is put at the end of the list each turn, no need to check it (2nd turn, means last 2 elements are sorted, 3rd turn means 3 last elemenets are sorted for sure etc.)
     if swapped == False:
         print("List has been sorted")
         break
