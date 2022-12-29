0b7d08ed287e67469152e9c6a00e3b5cb07f5bf4
t-mak <makowski.t91@gmail.com>
2022-12-30 02:59:38 +0900

    Add files via upload

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,28 +1,30 @@
-
-given_array = [3, 1, 5, 7, 2, 4, 4, 4, 9, 8, 7]
-
-length_array = len(given_array)
-turn = 0
-
-while (length_array>1):
-    swapped = False
-    turn = turn + 1
-    for j in range(0, length_array-1):
-        if given_array[j] > given_array[j+1]:
-            temp = given_array[j]
-            print()
-            print("While loop iteration: " + str(turn))
-            print("Swapping " + str(given_array[j]) + " with " + str(given_array[j+1]))
-            given_array[j] = given_array[j+1]
-            given_array[j+1] = temp
-            print(given_array)
-            swapped = True
-    length_array = length_array - 1 #since the highest value element is put at the end of the list each turn, no need to check it (2nd turn, means last 2 elements are sorted, 3rd turn means 3 last elemenets are sorted for sure etc.)
-    if swapped == False:
-        print("List has been sorted")
-        break
+def sort_helper(given_array):
     print()
-    print("*********************************")
-print("While loop iterations needed: " + str(turn))
+    print("Sorting an array: " + str(given_array))
+    sorted_array = sort_array(given_array)
+    print("Sorting result: " + str(sorted_array))
+
+def sort_array(given_array):
+    length_array = len(given_array)
+    while (length_array>1):
+        swapped = False
+        for j in range(0, length_array-1):
+            if given_array[j] > given_array[j+1]:
+                temp = given_array[j]
+                given_array[j] = given_array[j+1]
+                given_array[j+1] = temp
+                swapped = True
+        length_array = length_array - 1 #since the highest value element is put at the end of the list each turn, no need to check it (2nd turn, means last 2 elements are sorted, 3rd turn means 3 last elemenets are sorted for sure etc.)
+        if not swapped:
+            break
+    return given_array
+
+array1 = [3, 1, 5, 7, 2, 4, 4, 4, 9, 8, 7]
+array2 = [1560, 3410, 10, 2033]
+array3 = [3, -10, 15, -93, 1, -13, 5]
+sort_helper(array1)
+sort_helper(array2)
+sort_helper(array3)
+
 
 
